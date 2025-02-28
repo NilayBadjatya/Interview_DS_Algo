caef651208ccbdc28da5812f1f342187a62f5cc0
NilayBadjatya <147936252+NilayBadjatya@users.noreply.github.com>
2025-02-28 11:09:18 +0530

    Create Print Longest Common Subsequence.cpp

---
+++ Dynamic Programming/LCS Pattern/Print Longest Common Subsequence.cpp
@@ -0,0 +1,35 @@
+string LCS(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j < m + 1; j++) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        string lcs = "";
+        int i = n, j = m;
+        while (i > 0 && j > 0) {
+            if (s1[i - 1] == s2[j - 1]) {
+                lcs.push_back(s1[i - 1]);
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+        reverse(lcs.begin(), lcs.end());
+        return lcs;
+    }
