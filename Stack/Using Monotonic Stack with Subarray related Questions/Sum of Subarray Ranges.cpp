9a348845325a4180ea5e82d19bcde3b915407bfc
NilayBadjatya <147936252+NilayBadjatya@users.noreply.github.com>
2025-04-04 18:32:43 +0530

    Create Sum of Subarray Ranges.cpp

---
+++ Stack/Using Monotonic Stack with Subarray related Questions/Sum of Subarray Ranges.cpp
@@ -0,0 +1,102 @@
+typedef long long ll;
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> nsl(n), nsr(n), nll(n), nlr(n);
+        stack<int> st;
+        //Next Smaller To Left
+        for(int i = 0; i < n; i++){
+            if(!st.empty() && nums[st.top()] <= nums[i]){
+                nsl[i] = st.top();
+            }else if(!st.empty() && nums[st.top()] > nums[i]){
+                while(!st.empty() && nums[st.top()] > nums[i]){
+                    st.pop();
+                }
+                if(st.empty()){
+                    nsl[i] = -1;
+                }else{
+                    nsl[i] = st.top();
+                }
+            }else{
+                nsl[i] = -1;
+            }
+            st.push(i);
+        }
+        while (!st.empty()) {
+            st.pop();
+        }
+        //Next Smaller To Right
+        for(int i = n-1; i >= 0; i--){
+            if(!st.empty() && nums[st.top()] < nums[i]){
+                nsr[i] = st.top();
+            }else if(!st.empty() && nums[st.top()] >= nums[i]){
+                while(!st.empty() && nums[st.top()] >= nums[i]){
+                    st.pop();
+                }
+                if(st.empty()){
+                    nsr[i] = n;
+                }else{
+                    nsr[i] = st.top();
+                }
+            }else{
+                nsr[i] = n;
+            }
+            st.push(i);
+        }
+        while (!st.empty()) {
+            st.pop();
+        }
+        //Next Larger To Left
+        for(int i = 0; i < n; i++){
+            if(!st.empty() && nums[st.top()] >= nums[i]){
+                nll[i] = st.top();
+            }else if(!st.empty() && nums[st.top()] < nums[i]){
+                while(!st.empty() && nums[st.top()] < nums[i]){
+                    st.pop();
+                }
+                if(st.empty()){
+                    nll[i] = -1;
+                }else{
+                    nll[i] = st.top();
+                }
+            }else{
+                nll[i] = -1;
+            }
+            st.push(i);
+        }
+        while (!st.empty()) {
+            st.pop();
+        }
+        //Next Larger To Left
+        for(int i = n-1; i >= 0; i--){
+            if(!st.empty() && nums[st.top()] > nums[i]){
+                nlr[i] = st.top();
+            }else if(!st.empty() && nums[st.top()] <= nums[i]){
+                while(!st.empty() && nums[st.top()] <= nums[i]){
+                    st.pop();
+                }
+                if(st.empty()){
+                    nlr[i] = n;
+                }else{
+                    nlr[i] = st.top();
+                }
+            }else{
+                nlr[i] = n;
+            }
+            st.push(i);
+        }
+        ll maxi = 0, mini = 0;
+        for(int i = 0; i < n; i++){
+            ll leftS = i - nsl[i];
+            ll rightS = nsr[i] - i;
+            ll total = leftS * rightS;
+            mini += (total * (ll)nums[i]);
+            leftS = i - nll[i];
+            rightS = nlr[i] - i;
+            total = leftS * rightS;
+            maxi += (total * (ll)nums[i]);
+        }
+        return maxi - mini;
+    }
+};
