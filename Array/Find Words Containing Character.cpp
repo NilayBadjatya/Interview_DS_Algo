1f3a44b4403d1f43e491129af74743ca7755a9ed
Nilay Badjatya <147936252+NilayBadjatya@users.noreply.github.com>
2025-05-24 18:20:41 +0530

    Update Find Words Containing Character.cpp

--- Array/Find Words Containing Character.cpp
+++ Array/Find Words Containing Character.cpp
@@ -1,3 +1,5 @@
+// Approach 1:
+
 class Solution {
 public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
@@ -11,6 +13,26 @@ public:
                 }
             }
         }
+        return indices;
+    }
+};
+
+// Approach 2:
+
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        int n = words.size();
+        vector<int> indices;
+        auto lamda = [x](char ch){ // by doing lamda capture we can use the character x inside this
+            return ch == x;
+        };
+        for(int i = 0; i < n; i++){
+            if(any_of(words[i].begin(), words[i].end(), lamda)){
+                indices.push_back(i);
+            }
+        }
+        // Learnt new technique in STL -> any_of(), all_of(), none_of() from codestorywithMIK.
         return indices;
     }
 };
