2042f65bd2d54732e5a5b598eb06f5a19451feac
Nilay Badjatya <147936252+NilayBadjatya@users.noreply.github.com>
2025-06-05 09:17:30 +0530

    Update Lexicographically Smallest Equivalent String.cpp

--- Graph/DFS/Lexicographically Smallest Equivalent String.cpp
+++ Graph/DFS/Lexicographically Smallest Equivalent String.cpp
@@ -40,3 +40,50 @@ public:
         return res;
     }
 };
+
+//--------------------------------JAVA----------------------------------
+
+class Solution {
+
+    void dfs(Map<Character, List<Character>> adj, char c, boolean[] visited){
+        visited[c - 'a'] = true;
+        for(char v : adj.getOrDefault(c, new ArrayList<>())){
+            if(!visited[v - 'a']){
+                dfs(adj, v, visited);
+            }
+        }
+    }
+
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        int n = s1.length();
+        int m = baseStr.length();
+
+        Map<Character, List<Character>> adj = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            char a = s1.charAt(i);
+            char b = s2.charAt(i);
+
+            adj.computeIfAbsent(a, k -> new ArrayList<>()).add(b);
+            adj.computeIfAbsent(b, k -> new ArrayList<>()).add(a);
+        }
+
+        String res = "";
+        for(int i = 0; i < m; i++){
+            boolean[] visited = new boolean[26];
+            dfs(adj, baseStr.charAt(i), visited);
+            char c = baseStr.charAt(i);
+            for(int j = 0; j < 26; j++){
+                if(visited[j]){
+                    c = (char)(j + 'a');
+                    break;
+                }
+            }
+            if(c < baseStr.charAt(i)){
+                res += c;
+            }else{
+                res += baseStr.charAt(i);
+            }
+        }
+        return res;
+    }
+}
