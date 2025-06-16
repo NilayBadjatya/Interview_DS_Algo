63f8824957a0d58be4693b5ab330a99f829683eb
Nilay Badjatya <147936252+NilayBadjatya@users.noreply.github.com>
2025-06-16 15:43:37 +0530

    Update Combination Sum.cpp

--- Backtracking/Combination Sum.cpp
+++ Backtracking/Combination Sum.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
 
-    void solve(vector<int>& nums, int i, int sum, int target, vector<int> temp, vector<vector<int>>& list){
-        if(sum > target || i == nums.size()) return;
-        if(sum == target){
-            sort(temp.begin(), temp.end());
-            if(find(list.begin(), list.end(), temp) == list.end())
-                list.push_back(temp);
+    void solve(vector<int>& nums, int i, int target, vector<int> temp, vector<vector<int>>& list){
+        if(target < 0) return;
+        if(target == 0){
+            list.push_back(temp);
             return;
         }
         for(int j = i; j < nums.size(); j++){
-            sum += nums[j];
             temp.push_back(nums[j]);
-            solve(nums, i, sum, target, temp, list);
+            solve(nums, j, target - nums[j], temp, list);
             temp.pop_back();
-            sum -= nums[j];
         }
     }
 
     vector<vector<int>> combinationSum(vector<int>& nums, int target) {
         vector<int> temp;
         vector<vector<int>> list;
-        solve(nums, 0, 0, target, temp, list);
+        solve(nums, 0, target, temp, list);
         return list;
     }
 };
