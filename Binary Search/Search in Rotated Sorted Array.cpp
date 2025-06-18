d6439f267c2145f9ca66334dbd80f25eea884716
Nilay Badjatya <147936252+NilayBadjatya@users.noreply.github.com>
2025-06-18 15:12:11 +0530

    Update Search in Rotated Sorted Array.cpp

--- Binary Search/Search in Rotated Sorted Array.cpp
+++ Binary Search/Search in Rotated Sorted Array.cpp
@@ -36,3 +36,45 @@ public:
         return x;
     }
 };
+
+// Another Version
+
+class Solution {
+public:
+
+    int binarySearch(vector<int>& nums, int l, int r, int target){
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] > target){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0, r = n-1;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(nums[0] > nums[mid]){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        int pivot = r;
+        int idx1 = binarySearch(nums, 0, pivot, target);
+        int idx2 = binarySearch(nums, pivot+1, n-1, target);
+        if(idx1 == -1 && idx2 == -1){
+            return -1;
+        }else if(idx1 == -1){
+            return idx2;
+        }
+        return idx1;
+    }
+};
