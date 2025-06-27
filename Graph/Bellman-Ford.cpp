a0e80fda3ef574fa2139123f76ebada1c8d1843d
Nilay Badjatya <147936252+NilayBadjatya@users.noreply.github.com>
2025-06-27 20:50:23 +0530

    Create Bellman-Ford.cpp

---
+++ Graph/Bellman-Ford.cpp
@@ -0,0 +1,29 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        // Code here
+        vector<int> result(V, 1e8);
+        result[src] = 0;
+        for(int i = 0; i < V-1; i++){
+            for(auto& e : edges){
+                int u = e[0];
+                int v = e[1];
+                int w = e[2];
+                if(result[u] != 1e8 && result[u] + w < result[v]){
+                    result[v] = result[u] + w;
+                }
+            }
+        }
+        for(auto& e : edges){
+                int u = e[0];
+                int v = e[1];
+                int w = e[2];
+                if(result[u] != 1e8 && result[u] + w < result[v]){
+                    return {-1};
+                }
+            }
+        return result;
+    }
+};
