b9e4b2c7d808b4b9f7fca8354c613b8aa2253f48
Nilay Badjatya <147936252+NilayBadjatya@users.noreply.github.com>
2025-06-27 20:21:32 +0530

    Create Dijkstra Algorithm.cpp

---
+++ Graph/Dijkstra Algorithm.cpp
@@ -0,0 +1,35 @@
+// User Function Template
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Code here
+        unordered_map<int, vector<vector<int>>> adj;
+        for(auto& e : edges){
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
+        
+        queue<pair<int, int>> q;
+        q.push({0, src});
+        vector<int> result(V, INT_MAX);
+        result[src] = 0;
+        while(!q.empty()){
+            int w = q.front().first;
+            int node = q.front().second;
+            q.pop();
+            // visited[node] = true;
+            for(auto& v : adj[node]){
+                int curr = v[1];
+                int currNode = v[0];
+                if(result[currNode] > w + curr){
+                    result[currNode] = w + curr;
+                    q.push({result[currNode], currNode});
+                }
+            }
+        }
+        return result;
+    }
+};
