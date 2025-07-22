0d35efb82aab973fdab2a9293fd32bc7d83ab85f
Nilay Badjatya <147936252+NilayBadjatya@users.noreply.github.com>
2025-07-22 09:18:47 +0530

    Update Smallest Positive Missing.cpp

--- Sorting/Smallest Positive Missing.cpp
+++ Sorting/Smallest Positive Missing.cpp
@@ -1,3 +1,25 @@
+Time Complexity - O(N)
+class Solution {
+  public:
+    int missingNumber(vector<int> &arr) {
+        // code here
+        int miss = 1, n = arr.size();
+        // sort(arr.begin(), arr.end());
+        for(int i = 0; i < n; i++){
+            while(arr[i] >= 1 && arr[i] <= n && arr[i] != arr[arr[i] - 1]){
+                swap(arr[i], arr[arr[i] - 1]);
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(arr[i] != i+1){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+};
+
+Time Complexity - O(NLogN)
 class Solution {
   public:
     int missingNumber(vector<int> &arr) {
