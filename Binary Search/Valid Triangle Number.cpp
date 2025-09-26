7cbdaedf25e548b8f9c8ea5f5f230b972fdcc54b
Nilay Badjatya <147936252+NilayBadjatya@users.noreply.github.com>
2025-09-26 18:08:32 +0530

    Update Valid Triangle Number.cpp

--- Binary Search/Valid Triangle Number.cpp
+++ Binary Search/Valid Triangle Number.cpp
@@ -15,3 +15,36 @@ public:
         return ans;
     }
 };
+
+// JAVA CODE
+
+class Solution {
+
+    public int lower_bound(int[] nums, int k){
+        int n = nums.length;
+        int low = 0, high = n-1, idx = -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] < k){
+                idx = mid;
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return idx;
+    }
+
+    public int triangleNumber(int[] nums) {
+        int n = nums.length, ans = 0;
+        Arrays.sort(nums);
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                int thirdSide = nums[i] + nums[j];
+                int idx = lower_bound(nums, thirdSide);
+                ans += Math.max(idx - j, 0);
+            }
+        }
+        return ans;
+    }
+}
